cf60df0d6d96947bdd9560a96792f974c9cf21ae
Leonard Shtika <leoshtika@gmail.com>
2016-07-24 19:19:32 +0300

    Add missing yii\helpers\Html in passwordResetToken-text.php

--- common/mail/passwordResetToken-text.php
+++ common/mail/passwordResetToken-text.php
@@ -3,6 +3,8 @@
 /* @var $this yii\web\View */
 /* @var $user common\models\User */
 
+use yii\helpers\Html;
+
 $resetLink = Yii::$app->urlManager->createAbsoluteUrl(['site/reset-password', 'token' => $user->password_reset_token]);
 ?>
 Hello <?= Html::encode($user->full_name) ?>,
