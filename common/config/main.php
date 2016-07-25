a0c947258d875d8624b422510935f19062918cc6
Leonard Shtika <leoshtika@gmail.com>
2016-07-25 21:16:03 +0300

    Resolve #1: Configure 'mailer' with swiftmailer

--- common/config/main.php
+++ common/config/main.php
@@ -26,5 +26,24 @@ return [
                 ],
             ],
         ],
+        /**
+         * Copy the following commented 'mailer' property to the 'main-local.php'
+         * and configure the 'transport' property with your data
+         * 
+         * If you want to send the emails to 'common/mail' folder for debbuging
+         * set 'useFileTransport' to false and delete transport property
+         */
+//        'mailer' => [
+//            'class' => 'yii\swiftmailer\Mailer',
+//            'viewPath' => '@common/mail',
+//            'useFileTransport' => false,
+//            'transport' => [
+//                'class' => 'Swift_SmtpTransport',
+//                'host' => 'mail.xxxxxxxx.com',
+//                'username' => 'xxx@xxxxxxxx.com',
+//                'password' => 'xxxxxxxx',
+//                'port' => '25'
+//            ],
+//        ],
     ],
 ];
