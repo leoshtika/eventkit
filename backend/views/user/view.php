2838e0529e526732d578a738d10335124c679778
Leonard Shtika <leoshtika@gmail.com>
2016-07-30 21:04:06 +0300

    Resolve #5: Improve 'user/view' in backend

--- backend/views/user/view.php
+++ backend/views/user/view.php
@@ -6,24 +6,27 @@ use yii\widgets\DetailView;
 /* @var $this yii\web\View */
 /* @var $model common\models\User */
 
-$this->title = $model->id;
+$this->title = $model->full_name;
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Users'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="user-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>
-        <?= Html::a(Yii::t('app', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a(Yii::t('app', 'Delete'), ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => Yii::t('app', 'Are you sure you want to delete this item?'),
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
+    <div class="row">
+        <div class="col-sm-6 hidden-xs">
+            <h1 class="allpages_title"><?= Html::encode($this->title) ?></h1>
+        </div>
+        <div class="col-sm-6 text-right allpages_buttons">
+            <?= Html::a('<span class="glyphicon glyphicon-pencil"></span> '.Yii::t('app', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-success']) ?>
+            <?= Html::a('<span class="glyphicon glyphicon-trash"></span> '.Yii::t('app', 'Delete'), ['delete', 'id' => $model->id], [
+                'class' => 'btn btn-danger',
+                'data' => [
+                    'confirm' => Yii::t('app', 'Are you sure you want to delete this item?'),
+                    'method' => 'post',
+                ],
+            ]) ?>
+        </div>
+    </div>
 
     <?= DetailView::widget([
         'model' => $model,
@@ -31,13 +34,16 @@ $this->params['breadcrumbs'][] = $this->title;
             'id',
             'full_name',
             'email:email',
-            'auth_key',
-            'password_hash',
-            'password_reset_token',
-            'status',
-            'role',
-            'created_at',
-            'updated_at',
+            [
+                'attribute' => 'status',
+                'value'=> $model->getStatusLabel(),
+            ],
+            [
+                'attribute' => 'role',
+                'value'=> $model->getRoleLabel(),
+            ],
+            'created_at:datetime',
+            'updated_at:datetime',
         ],
     ]) ?>
 
