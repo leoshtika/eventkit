36b2f0d2e17a9ac8328b0c7b3df37a7c28fc3f0b
Leonard Shtika <leoshtika@gmail.com>
2016-07-30 21:20:47 +0300

    Resolve #6: Improve 'user/update' in backend

--- backend/views/user/_form.php
+++ backend/views/user/_form.php
@@ -13,15 +13,31 @@ use yii\widgets\ActiveForm;
     <?php $form = ActiveForm::begin(); ?>
 
     <?= $form->field($model, 'full_name')->textInput(['maxlength' => true]) ?>
-
     <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'status')->textInput() ?>
-
-    <?= $form->field($model, 'role')->textInput() ?>
+    
+    <?php if ($model->isNewRecord) : ?>
+        <?= $form->field($model, 'password')->passwordInput(['maxlength' => true]) ?>
+    <?php else : ?>
+        <?= $form->field($model, 'newPassword')->passwordInput([
+            'maxlength' => true,
+            'placeholder' => Yii::t('app', 'Leave this field empty if you do NOT want to change the password'),
+            'autocomplete' => 'off',
+        ]) ?>
+    <?php endif; ?>
+    
+    <?= $form->field($model, 'role')->radioList($model->getRoleList(), [
+        'class' => 'radio_list',
+    ]) ?>
+    
+    <?= $form->field($model, 'status')->radioList($model->getStatusList(), [
+        'class' => 'radio_list',
+    ]) ?>
 
     <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+        <?= Html::submitButton($model->isNewRecord ? 
+                '<span class="glyphicon glyphicon-ok" aria-hidden="true"></span> '.Yii::t('app', 'Create') : 
+                '<span class="glyphicon glyphicon-ok" aria-hidden="true"></span> '.Yii::t('app', 'Update'), 
+            ['class' => 'btn btn-primary']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
--- backend/views/user/update.php
+++ backend/views/user/update.php
@@ -5,9 +5,7 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model common\models\User */
 
-$this->title = Yii::t('app', 'Update {modelClass}: ', [
-    'modelClass' => 'User',
-]) . $model->id;
+$this->title = Yii::t('app', 'Update user').' #'.$model->id.': '.$model->full_name;
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Users'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = Yii::t('app', 'Update');
