618ca797353e9a1d0060d26977cfb807ea3dbea0
Leonard Shtika <leoshtika@gmail.com>
2016-07-30 14:20:51 +0300

    Set 'autocomplete' to off in signup form

--- frontend/views/site/signup.php
+++ frontend/views/site/signup.php
@@ -20,11 +20,23 @@ $this->params['breadcrumbs'][] = $this->title;
         <div class="col-lg-5">
             <?php $form = ActiveForm::begin(['id' => 'form-signup']); ?>
 
-                <?= $form->field($model, 'full_name')->textInput(['autofocus' => true]) ?>
+                <?= $form->field($model, 'full_name', [
+                    'inputOptions' => [
+                        'autocomplete' => 'off',
+                    ],
+                ])->textInput(['autofocus' => true]) ?>
 
-                <?= $form->field($model, 'email') ?>
+                <?= $form->field($model, 'email', [
+                    'inputOptions' => [
+                        'autocomplete' => 'off',
+                    ],
+                ]) ?>
 
-                <?= $form->field($model, 'password')->passwordInput() ?>
+                <?= $form->field($model, 'password', [
+                    'inputOptions' => [
+                        'autocomplete' => 'off',
+                    ],
+                ])->passwordInput() ?>
             
                 <?= $form->field($model, 'verifyCode')->widget(Captcha::className(), [
                     'template' => '<div class="row"><div class="col-md-3 captcha">{image}</div><div class="col-md-6">{input}</div></div>',
