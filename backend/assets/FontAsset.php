72ea13eae143f465ee20a7a5d68b0456b81a9bfa
Leonard Shtika <leoshtika@gmail.com>
2016-08-14 01:32:14 +0300

    Add 'Roboto Condensed' font and some minor changes

---
+++ backend/assets/FontAsset.php
@@ -0,0 +1,16 @@
+<?php
+namespace backend\assets;
+
+use yii\web\AssetBundle;
+
+class FontAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+        '//fonts.googleapis.com/css?family=Roboto+Condensed&subset=latin,greek'
+    ];
+    public $cssOptions = [
+        'type' => 'text/css',
+    ];
+}
