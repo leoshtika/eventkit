f007ed7bdddabc37098a756890b9130489d6703c
Leonard Shtika <leoshtika@gmail.com>
2016-08-15 01:22:29 +0300

    Improve 'user/account' view by changing column ordering

--- frontend/views/user/account.php
+++ frontend/views/user/account.php
@@ -18,33 +18,35 @@ $this->params['breadcrumbs'][] = $this->title;
     <p>
         <?= Yii::t('app', 'Here you can change all your account details.') ?>
     </p>
-    <div>
-        <div class="profile">
-            <?= Gravatar::widget([
-                    'email' => $model->email,
-                    'defaultImage' => 'mm',
-                    'options' => [
-                        'alt' => $model->full_name,
-                        'class' => 'img-rounded'
-                    ],
-                    'size' => 130
-            ]); ?>
-        </div>
-        <div class="clearfix visible-xs"></div>
-        <h2><?= $model->full_name ?></h2>
-        <p>
-            <?= Yii::t('app', 'Member since: <strong>{0, date}</strong>', $model->created_at) ?><br>
-            <?= Yii::t('app', 'Last update: <strong>{0, date}</strong>', $model->updated_at) ?><br>
-            <?= Yii::t('app', 'Role') ?>: <strong><?= $model->roleLabel ?></strong><br>
-            <?= Yii::t('app', 'Change avatar via {gravatar}', [
-                'gravatar' => Html::a('Gravatar', 'http://gravatar.com', ['target'=>'_blank']),
-            ]) ?>
-        </p>
-        <div class="clearfix"></div>
-    </div>
-    <hr>
     <div class="row">
-        <div class="col-lg-5">
+        <div class="col-lg-6 col-lg-offset-1 col-lg-push-5">
+            <div>
+            <div class="profile">
+                <?= Gravatar::widget([
+                        'email' => $model->email,
+                        'defaultImage' => 'mm',
+                        'options' => [
+                            'alt' => $model->full_name,
+                            'class' => 'img-rounded'
+                        ],
+                        'size' => 130
+                ]); ?>
+            </div>
+            <div class="clearfix visible-xs"></div>
+            <h2><?= $model->full_name ?></h2>
+            <p>
+                <?= Yii::t('app', 'Member since: <strong>{0, date}</strong>', $model->created_at) ?><br>
+                <?= Yii::t('app', 'Last update: <strong>{0, date}</strong>', $model->updated_at) ?><br>
+                <?= Yii::t('app', 'Role') ?>: <strong><?= $model->roleLabel ?></strong><br>
+                <?= Yii::t('app', 'You can change your profile picture on {gravatar}', [
+                    'gravatar' => Html::a('Gravatar', 'http://gravatar.com', ['target'=>'_blank']),
+                ]) ?>
+            </p>
+            <div class="clearfix"></div>
+            <hr class="hidden-lg">
+            </div>
+        </div>
+        <div class="col-lg-5 col-lg-pull-7">
             <?php $form = ActiveForm::begin(); ?>
             
                 <?= $form->field($model, 'full_name')->textInput(['maxlength' => true]) ?>
