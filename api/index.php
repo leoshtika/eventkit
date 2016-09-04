f5fdfb9c9312d6d44d1560666fb78708f4e0b155
Leonard Shtika <leoshtika@gmail.com>
2016-09-04 17:32:56 +0300

    Enable production mode in API responses

--- api/index.php
+++ api/index.php
@@ -1,6 +1,9 @@
 <?php
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
+// For more details debugging in development mode replace these 2 lines with the following 2
+//defined('YII_DEBUG') or define('YII_DEBUG', true);
+//defined('YII_ENV') or define('YII_ENV', 'dev');
+defined('YII_DEBUG') or define('YII_DEBUG', false);
+defined('YII_ENV') or define('YII_ENV', 'prod');
 
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
