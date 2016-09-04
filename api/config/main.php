dcf1aeadc22ba5333aadcde39c77325b03ddfd68
Leonard Shtika <leoshtika@gmail.com>
2016-09-04 13:22:21 +0300

    Scaffold an api application

---
+++ api/config/main.php
@@ -0,0 +1,54 @@
+<?php
+$params = array_merge(
+    require(__DIR__ . '/../../common/config/params.php'),
+    require(__DIR__ . '/../../common/config/params-local.php'),
+    require(__DIR__ . '/params.php')
+);
+
+return [
+    'id' => 'app-api',
+    'basePath' => dirname(__DIR__),
+    'controllerNamespace' => 'api\controllers',
+    'bootstrap' => ['log'],
+    'components' => [
+        'user' => [
+            'identityClass' => 'common\models\User',
+            'enableAutoLogin' => false,
+            'enableSession' => false,
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'request' => [
+            'parsers' => [
+                'application/json' => 'yii\web\JsonParser',
+            ]
+        ],
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'enableStrictParsing' => true,
+            'showScriptName' => false,
+            'rules' => [
+                [
+                    'class' => 'yii\rest\UrlRule', 
+                    'controller' => ['user'],
+                    'pluralize' => false,
+                    'extraPatterns' => [
+                        'OPTIONS login' => 'options',
+                        'OPTIONS create' => 'options',
+                        'POST login' => 'login',
+                        'POST create' => 'create',
+                        'GET account' => 'account',
+                    ]
+                ],
+            ],
+        ],
+    ],
+    'params' => $params,
+];
