b406e70467cd9d8a215cfc995f3947e421288360
Leonard Shtika <leoshtika@gmail.com>
2016-09-23 20:00:45 +0300

    Fix typo in backend session

--- backend/views/session/update.php
+++ backend/views/session/update.php
@@ -5,7 +5,7 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model common\models\Session */
 
-$this->title = Yii::t('app', 'Update user').' #'.$model->id.': '.$model->title;
+$this->title = Yii::t('app', 'Update session').' #'.$model->id.': '.$model->title;
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Sessions'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->title, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = Yii::t('app', 'Update');
