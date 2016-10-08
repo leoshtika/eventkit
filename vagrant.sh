37929365602921c8398378ddd97b5cdd5ab2b03a
Leonard Shtika <leoshtika@gmail.com>
2016-10-08 17:49:50 +0300

    Download and configure 'adminer.php' in Vagrant.sh

--- vagrant.sh
+++ vagrant.sh
@@ -33,7 +33,7 @@ apt-get -y install mysql-server libapache2-mod-auth-mysql
 # PHP 5.5 (this is the default and don't need ppa:ondrej/php)
 # apt-get -y install php5 libapache2-mod-php5 php5-mcrypt curl php5-curl php5-intl php5-imagick php5-mysql
 # PHP 7.0 (from ppa:ondrej/php)
-apt-get -y install php7.0 libapache2-mod-php7.0 php7.0-mcrypt curl php7.0-curl php7.0-intl php7.0-imagick php7.0-mysql php7.0-mbstring php7.0-xml
+apt-get -y install php7.0 libapache2-mod-php7.0 php7.0-mcrypt curl php7.0-curl php7.0-intl php7.0-imagick php7.0-mysql php7.0-mbstring php7.0-xml php7.0-zip
 
 # Install Composer
 if [ ! -f /usr/local/bin/composer ]; then
@@ -45,11 +45,20 @@ fi
 echo "Installing Composer Asset Plugin"
 composer global require "fxp/composer-asset-plugin:~1.1.1"
 
-# Restart Apache
-service apache2 restart
-
 # Add an alias for codecept
 echo "alias codecept='php /vagrant/vendor/bin/codecept'" >> /home/vagrant/.bashrc
+
+# Download and configure 'adminer.php' to manage the MySQL database
+if [ ! -f /usr/share/adminer.php ]; then
+    wget -q -O adminer.php https://www.adminer.org/static/download/4.2.5/adminer-4.2.5-en.php
+    mv adminer.php /usr/share/adminer.php
+    
+    # Create an alias for adminer, example: http://localhost:4000/adminer 
+    echo "alias /adminer '/usr/share/adminer.php'" >> /etc/apache2/sites-available/000-default.conf
+fi
+
+# Restart Apache
+service apache2 restart
 
 echo "================================"
 echo "Your LAMP stack is ready for use"
