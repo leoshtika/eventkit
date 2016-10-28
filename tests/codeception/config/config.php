61962342aaa5af52b5cea53994d2783e7f0d9a05
Leonard Shtika <leoshtika@gmail.com>
2016-10-28 13:01:35 +0300

    Update codeception and remove the deprecated yii2-codeception

--- tests/codeception/config/config.php
+++ tests/codeception/config/config.php
@@ -14,7 +14,10 @@ return [
     ],
     'components' => [
         'db' => [
-            'dsn' => 'mysql:host=localhost;dbname=yii2_advanced_tests',
+            'dsn' => 'mysql:host=localhost;dbname=eventkit_test',
+            'username' => 'root',
+            'password' => 'pass123',
+            'charset' => 'utf8',
         ],
         'mailer' => [
             'useFileTransport' => true,
