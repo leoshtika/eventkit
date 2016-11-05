bb488dc6ce6fce0f1a47ca8d8fb21e83f057f2ee
Leonard Shtika <leoshtika@gmail.com>
2016-11-05 16:57:04 +0200

    Change title in about page

--- frontend/views/site/about.php
+++ frontend/views/site/about.php
@@ -4,7 +4,7 @@
 
 use yii\helpers\Html;
 
-$this->title = Yii::t('app', 'About');
+$this->title = Yii::t('app', 'What is EventKit?');
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
