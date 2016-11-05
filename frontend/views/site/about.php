976628b22113d9c8227012b8e917cc5a1eeac40c
Leonard Shtika <leoshtika@gmail.com>
2016-11-05 16:53:00 +0200

    Add rows in about page

--- frontend/views/site/about.php
+++ frontend/views/site/about.php
@@ -10,21 +10,33 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
     <hr>
-    <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
-        standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to 
-        make a type specimen book. 
-    </p>
-    <p>
-        It has survived not only five centuries, but also the leap into electronic 
-        typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
-        sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker 
-        including versions of Lorem Ipsum.
-    </p>
-    <p>
-        It has survived not only five centuries, but also the leap into electronic 
-        typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
-        sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker 
-        including versions of Lorem Ipsum.
-    </p>
+    <div class="row">
+        <div class="col-md-4">
+            <h3>What is Lorem Ipsum?</h3>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
+            standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to 
+            make a type specimen book. Contrary to popular belief, Lorem Ipsum is not simply random text.</p>
+        </div>
+        <div class="col-md-4">
+            <h3>Where does it come from?</h3>
+            It has survived not only five centuries, but also the leap into electronic 
+            typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
+            sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker 
+            including versions of Lorem Ipsum.
+        </div>
+        <div class="col-md-4">
+            <h3>Why do we use it?</h3>
+            It has survived not only five centuries, but also the leap into electronic 
+            typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
+            sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker 
+            including versions of Lorem Ipsum.
+        </div>
+        <div class="col-md-12">
+            <h3>Where can I get some?</h3>
+            It has survived not only five centuries, but also the leap into electronic 
+            typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
+            sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker 
+            including versions of Lorem Ipsum.
+        </div>
+    </div>
 </div>
