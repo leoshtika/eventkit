025ab7a63e89f7a7fa76427f5a8ec85cfdfe56fb
Leonard Shtika <leoshtika@gmail.com>
2016-11-05 16:48:11 +0200

    Add some lorem ipsum text in about page

--- frontend/views/site/about.php
+++ frontend/views/site/about.php
@@ -10,4 +10,21 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
     <hr>
+    <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
+        standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to 
+        make a type specimen book. 
+    </p>
+    <p>
+        It has survived not only five centuries, but also the leap into electronic 
+        typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
+        sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker 
+        including versions of Lorem Ipsum.
+    </p>
+    <p>
+        It has survived not only five centuries, but also the leap into electronic 
+        typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
+        sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker 
+        including versions of Lorem Ipsum.
+    </p>
 </div>
