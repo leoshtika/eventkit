173616933849af62634c2257108dc188606ef51f
anna-d <anna-d@users.noreply.github.com>
2016-11-25 14:23:37 +0200

    Add fixture

---
+++ tests/codeception/common/fixtures/data/event.php
@@ -0,0 +1,3 @@
+return array(
+  array('id' => '9','title' => 'An event','starts' => '2016-11-23 10:00:00','ends' => '2016-11-23 13:00:00','location' => 'Thessaloniki','latitude' => NULL,'longitude' => NULL,'description' => 'This is an event.','created_at' => '1480076534','updated_at' => '1480076534')
+);
