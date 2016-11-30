d7ad178242128703923221a65ab0fadc8325c1e7
Leonard Shtika <leoshtika@gmail.com>
2016-11-30 19:55:59 +0200

    Create a basic structure for 'about' page

--- frontend/views/site/about.php
+++ frontend/views/site/about.php
@@ -8,35 +8,61 @@ $this->title = Yii::t('app', 'What is EventKit?');
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
-    <h1><?= Html::encode($this->title) ?></h1>
-    <hr>
+    <br><br>
     <div class="row">
-        <div class="col-md-4">
-            <h3>What is Lorem Ipsum?</h3>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
-            standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to 
-            make a type specimen book. Contrary to popular belief, Lorem Ipsum is not simply random text.</p>
+        <div class="col-md-7">
+            <h2 class="featurette-heading"><?= Html::encode($this->title) ?></h2>
+            <p class="lead">
+                It was popularised in the 1960s with the release of Letraset 
+                sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker 
+                including versions of Lorem Ipsum.
+            </p>
         </div>
-        <div class="col-md-4">
-            <h3>Where does it come from?</h3>
-            It has survived not only five centuries, but also the leap into electronic 
-            typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
-            sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker 
-            including versions of Lorem Ipsum.
+        <div class="col-md-5">
+            <?= Html::img('@web/images/web-app0.jpg', [
+                'class' => 'featurette-image img-responsive center-block',
+                'alt' => '',
+            ]) ?>
         </div>
-        <div class="col-md-4">
-            <h3>Why do we use it?</h3>
-            It has survived not only five centuries, but also the leap into electronic 
-            typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
-            sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker 
-            including versions of Lorem Ipsum.
+    </div>
+
+    <hr class="featurette-divider hidden-xs">
+
+    <div class="row">
+        <div class="col-md-7 col-md-push-5">
+            <h2 class="featurette-heading">Why do we use it?</h2>
+            <p class="lead">
+                It has survived not only five centuries, but also the leap into electronic 
+                typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
+                sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker 
+                including versions of Lorem Ipsum.
+            </p>
         </div>
-        <div class="col-md-12">
-            <h3>Where can I get some?</h3>
-            It has survived not only five centuries, but also the leap into electronic 
-            typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
-            sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker 
-            including versions of Lorem Ipsum.
+        <div class="col-md-5 col-md-pull-7">
+            <?= Html::img('@web/images/web-app1.jpg', [
+                'class' => 'featurette-image img-responsive center-block',
+                'alt' => '',
+            ]) ?>
+        </div>
+    </div>
+
+    <hr class="featurette-divider hidden-xs">
+
+    <div class="row">
+        <div class="col-md-7">
+            <h2 class="featurette-heading">Where can I get some?</h2>
+            <p class="lead">
+                It has survived not only five centuries, but also the leap into electronic 
+                typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
+                sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker 
+                including versions of Lorem Ipsum.
+            </p>
+        </div>
+        <div class="col-md-5">
+            <?= Html::img('@web/images/web-app2.jpg', [
+                'class' => 'featurette-image img-responsive center-block',
+                'alt' => '',
+            ]) ?>
         </div>
     </div>
 </div>
--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -10,7 +10,6 @@ $this->title = 'EventKit';
 
     <div class="jumbotron">
         <h1>Welcome to EventKit!</h1>
-
         <p class="lead">A conference management framework</p>
         <?= Html::a('Get started here', ['site/about'], [
             'class' => 'btn btn-lg btn-primary',
@@ -18,8 +17,11 @@ $this->title = 'EventKit';
         <br><br>
         <?= Html::a('View the GitHub project', 'https://github.com/leoshtika/eventkit') ?>
     </div>
-
-    <div class="body-content">
-
+    <div class="body-content text-center">
+        <?= Html::img('@web/images/main.jpg') ?><br><br>
+        <p>
+        It has survived not only five centuries, but also the leap into electronic 
+        typesetting, remaining essentially unchanged. 
+        </p>
     </div>
 </div>
